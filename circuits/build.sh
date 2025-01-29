1a75bb62f2a3d8e3ab3e42923beb1315ffa5dd3c
jananathan <janan.karur@gmail.com>
2025-01-29 22:28:52 +0530

    ultra-honk

--- circuits/build.sh
+++ circuits/build.sh
@@ -1,6 +1,6 @@
 set -e
-rm -rf target
 
+rm -rf target
 echo "Compiling circuit..."
 if ! nargo compile; then
   echo "Compilation failed. Exiting..."
@@ -11,15 +11,17 @@ echo "Gate count:"
 bb gates -b target/zkp.json | jq '.functions[0].circuit_size'
 
 echo "Generating vkey..."
-bb write_vk -b ./target/zkp.json
-bb contract
+bb write_vk_ultra_keccak_honk -b ./target/zkp.json -o ./target/vk -c ~/.bb-crs -v
+
+echo "Generating solidity verifier..."
+bb contract_ultra_honk -k ./target/vk -c ~/.bb-crs -o ./target/Verifier.sol -v
 
 echo "Generating proof..."
-nargo execute zkp
-bb prove -b ./target/zkp.json -w ./target/zkp.gz -o ./target/proof
+nargo execute
+bb prove_ultra_keccak_honk -b ./target/zkp.json -w ./target/zkp.gz -o ./target/proof -c ~/.bb-crs -v
+bb proof_as_fields_honk -k ./target/vk -p ./target/proof -c ~/.bb-crs -v
 
 echo "Verifying proof..."
-bb verify -k ./target/vk -p ./target/proof
-
+bb verify_ultra_keccak_honk -k ./target/vk -p ./target/proof -b ./target/zkp.json -c ~/.bb-crs -v
 
 echo "Done"
